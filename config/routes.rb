a373ec0e2963e8cc8725dc4c7d2168bec36cb459
hikusukih <sir.hikus@gmail.com>
2017-04-10 22:07:47 -0400

    Sanity commit
    Start implementing Facebook Auth, definitely need to abstract out the
    secrets and keys, and it’s broken now due to implementation of same

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
-  root 'home#index'
-
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  devise_for :users
-  resources :posts
+  devise_for :users, :controllers => { omniauth_callbacks: 'omniauth_callbacks' }
+ 
+  resources :users
+  match '/users/:id/finish_signup' => 'users#finish_signup', via: [:get, :patch], :as => :finish_signup
+
+ resources :posts
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'home#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
